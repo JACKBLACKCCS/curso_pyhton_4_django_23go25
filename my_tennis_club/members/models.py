4477e6749221790ceefb14a11d6e847fcc87f960
JACKBLACKCCS <2020velasquez@gmail.com>
2025-08-30 16:00:55 -0400

    Feat Finish administardor de Django: establecer campos para mostrar

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -1,5 +1,4 @@
 
-
 from django.db import models
 
 class Member(models.Model):
@@ -8,3 +7,5 @@ class Member(models.Model):
   phone = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
 
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
